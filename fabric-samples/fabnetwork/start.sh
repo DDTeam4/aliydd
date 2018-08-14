dab1b76b363ac43e0e4e1cf0d060822d7db19c90
gitydd <1143526818@qq.com>
2018-08-14 06:48:05 -0700

    fabnetwork-peers join the channel

--- fabric-samples/fabnetwork/start.sh
+++ fabric-samples/fabnetwork/start.sh
@@ -12,7 +12,7 @@ export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com peer1.org1.example.com peer2.org1.example.com couchdb
+docker-compose -f docker-compose.yml up -d
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
@@ -20,8 +20,15 @@ export FABRIC_START_TIMEOUT=10
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
-# Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
-# Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
 
+# Create the channel
+docker exec cli peer channel create -o orderer.example.com:7050 -c mychannel -f ./channel-artifacts/channel.tx
+
+# Join peer0.org1.example.com to the channel.
+docker exec cli peer channel join -b mychannel.block
+
+# Join peer1.org1.example.com to the channel.
+docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" cli peer channel join -b mychannel.block
+
+# Join peer2.org1.example.com to the channel.
+docker exec -e "CORE_PEER_ADDRESS=peer2.org1.example.com:7051" cli peer channel join -b mychannel.block
